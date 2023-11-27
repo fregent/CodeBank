f3b4c99647a3fbced0b7a9532243345f79c8f5d3
salma zakha <selma.zakha@outlook.fr>
2023-11-27 17:06:48 +0100

    base 2

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 Rails.application.routes.draw do
-  get 'directories/new'
-  get 'snippets/new'
+
+  resources :snippets do
+    resources :directories
+  end
+
+   resources :snippets
+
   devise_for :users
   root to: "pages#home"
-  
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
