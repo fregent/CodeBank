5900db507b1b0e3ef9d0948d227027f9909f77ba
Frederic REGENT <frederic.regent97@gmail.com>
2023-11-29 11:36:28 +0100

    snippet css

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
    resources :snippets
 
   devise_for :users
-  root to: "pages#home"
+  root to: "snippets#index"
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
