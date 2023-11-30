3db1082f7d72edd524eb419a3ea40ddd5399429a
Frederic REGENT <frederic.regent97@gmail.com>
2023-11-30 14:07:29 +0100

    navbar redirection to snippets index

--- app/models/directory.rb
+++ app/models/directory.rb
@@ -1,4 +1,4 @@
 class Directory < ApplicationRecord
   belongs_to :user
-  has_many :snippets
+  has_many :snippets, through: :user
 end
--- app/models/snippet.rb
+++ app/models/snippet.rb
@@ -1,4 +1,4 @@
 class Snippet < ApplicationRecord
   belongs_to :user
-  has_many :directories
+  has_many :directories, through: :user
 end
