a50ac7485724061e7b7adad5b41ef92cbf8c876f
Emeraude Ntondo <ntondo95130@gmail.com>
2023-11-30 16:19:49 +0100

    front-end

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   resources :directories
 
   devise_for :users
-  root to: "snippets#index"
+  root to: "pages#home"
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
