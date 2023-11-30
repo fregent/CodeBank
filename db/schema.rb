df68392e19032bd1fdf91592328dc571dd783327
salma zakha <selma.zakha@outlook.fr>
2023-11-30 10:54:10 +0100

    fixed merge

--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,4 @@
-g# This file is auto-generated from the current state of the database. Instead
+# This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
 #
@@ -20,10 +20,7 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_29_151450) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.boolean "private", default: true
-<<<<<<< HEAD
-=======
     t.integer "shared_count", default: 0
->>>>>>> master
     t.index ["user_id"], name: "index_directories_on_user_id"
   end
 
