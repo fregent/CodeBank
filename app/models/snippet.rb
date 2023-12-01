a68c2871eb7a1db5788d7e8348260cc63dfdcdb8
Andre Leroy <a.l.sismondino@outlook.fr>
2023-12-01 10:43:55 +0100

    okokok

--- app/models/snippet.rb
+++ app/models/snippet.rb
@@ -1,9 +1,12 @@
 class Snippet < ApplicationRecord
 
-include PgSearch::Model
-multisearchable against: [:title, :content, :language]
+  include PgSearch::Model
+  multisearchable against: [:title, :content, :language],
+                  using: {
+                    tsearch: { prefix: true, any_word: true, case_sensitive: false },
+                    trigram: { threshold: 0.1 }
+                  }
 
-belongs_to :user
-has_many :directories
-
+  belongs_to :user
+  has_many :directories
 end
